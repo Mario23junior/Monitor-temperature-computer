dbc6b41bb9de9c6f9bc66e63e9aa415ab84bb1fc
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-03 04:45:03 -0300

    inserindo esqueleto principal

--- src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
+++ src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
@@ -3,10 +3,16 @@ package com.project.computerTemperature.Controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.ScatterChart;
 
 public class FXMLComputerTemperatureController implements Initializable{
  
+
+    @FXML
+    private ScatterChart<?, ?> lineChart;
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
  		
