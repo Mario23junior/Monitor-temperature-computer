3160a736f8afcef0f0eef1eae2aa1c655e7047cd
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-03 05:09:49 -0300

    Criando computador

--- src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
+++ src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
@@ -6,6 +6,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import org.graalvm.compiler.core.amd64.AMD64ArithmeticLIRGenerator.Maths;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,7 +20,9 @@ public class FXMLComputerTemperatureController implements Initializable {
 	private ScatterChart<?, ?> lineChart;
 
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {
+	public void initialize(URL arg0, ResourceBundle rb) {
+		Thread th = new Thread(new TemperaureReader());
+		th.start();
 
 	}
 }
@@ -26,13 +31,24 @@ class TemperaureReader implements Runnable {
 	@Override
 	public void run() {
 		try {
-			Process proc = Runtime.getRuntime().exec("sensors");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-			
-			
+
+			while (true) {
+				
+				Pattern p = Pattern.compile("[+]...");
+				
+				Process proc = Runtime.getRuntime().exec("sensors");
+				BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+				String line = " ";
+				while ((line = reader.readLine()) != null) {
+					Maths m = p.matcher(line);
+					System.out.println(line);
+				}
+				Thread.sleep(1000);
+			}
+
 		} catch (Exception ex) {
-			Logger.getLogger(FXMLComputerTemperatureController.class.getName()).log(Level.SEVERE,null,ex);
- 		}
+			Logger.getLogger(FXMLComputerTemperatureController.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 }
