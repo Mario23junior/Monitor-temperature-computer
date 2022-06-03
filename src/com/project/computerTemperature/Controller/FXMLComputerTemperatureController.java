3a6262d4dcde7ed1e489041ca3eb89470cc24ed0
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-03 05:25:32 -0300

    Valores de leitura

--- src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
+++ src/com/project/computerTemperature/Controller/FXMLComputerTemperatureController.java
@@ -6,21 +6,29 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.graalvm.compiler.core.amd64.AMD64ArithmeticLIRGenerator.Maths;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 
 public class FXMLComputerTemperatureController implements Initializable {
 
 	@FXML
-	private ScatterChart<?, ?> lineChart;
+	private ScatterChart<Number, Number> lineChart;
+	XYChart.Series<Number, Number> series, series1,series2;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle rb) {
+		series = new XYChart.Series<>();
+		series.setName("Data 1");
+		series = new XYChart.Series<>();
+		series.setName("Data 2");
+		series = new XYChart.Series<>();
+		series.setName("Data 3");
+		
 		Thread th = new Thread(new TemperaureReader());
 		th.start();
 
@@ -35,15 +43,21 @@ class TemperaureReader implements Runnable {
 			while (true) {
 				
 				Pattern p = Pattern.compile("[+]...");
-				
+	
 				Process proc = Runtime.getRuntime().exec("sensors");
 				BufferedReader reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+				
 				String line = " ";
 				while ((line = reader.readLine()) != null) {
-					Maths m = p.matcher(line);
+					Matcher m = p.matcher(line);
+					if(m.find()) {
+						System.out.println("Matematica erro = "+ m.group());
+					}
+					
 					System.out.println(line);
 				}
-				Thread.sleep(1000);
+				System.out.println("-");
+				Thread.sleep(2000);
 			}
 
 		} catch (Exception ex) {
